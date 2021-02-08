d0f2914b20a1459372fff215fc7c0db5c4c43aae
fxnoob <fxnoob71@gmail.com>
2021-02-08 14:18:19 +0530

    approximation

--- approximation/1.js
+++ approximation/1.js
@@ -3,9 +3,18 @@
 function sum(upto) {
 	let s = 0;
 	for(let i = 1; i <= upto; i++) {
-		s += 1 / ( i * i );  
+		s += 1 / ( i * i );
 	}
 	return s;
 }
+// f(x) = x^2 - n [newtons method]
+function sqrt(n) {
+	let x = n / 2;
+	for (let i = 0; i < 1000; i++) {
+		x = x - (1/2) * (x - n/x);
+	}
+	return x;
+}
+
 
 console.log(sum(100))
